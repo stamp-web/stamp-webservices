d0db082205165d729859742c85052ce8fc54fead
jadrake75 <jadrake75@gmail.com>
2014-11-20 17:13:37 -0500

    Cost Basis reporting
    Fixes #33

--- app/services/reports.js
+++ app/services/reports.js
@@ -15,15 +15,20 @@ var sqlTrace = Logger.getLogger("sql");
 
 var report = function () {
     "use strict";
+
+    function generateFromTables() {
+        var sql = "FROM " + stamp.getTableName() + ' AS ' + stamp.getAlias() + ' ';
+        sql += 'JOIN ' + catalogueNumber.getTableName() + ' AS ' + catalogueNumber.getAlias() + ' ON ' + stamp.getAlias() + '.ID=' + catalogueNumber.getAlias() + '.STAMP_ID ';
+        sql += "LEFT JOIN " + ownership.getTableName() + ' AS ' + ownership.getAlias() + ' ON ' + stamp.getAlias() + '.ID=' + ownership.getAlias() + '.STAMP_ID ';
+        sql += "LEFT JOIN " + catalogue.getTableName() + ' AS ' + catalogue.getAlias() + ' ON ' + catalogueNumber.getAlias() + '.CATALOGUE_REF = ' + catalogue.getAlias() + '.ID ';
+        return sql;
+    }
     return {
         getCatalogueTotal: function ($filter, currency) {
             var defer = q.defer();
             connectionManager.getConnection("reports").then(function (connection) {
                 var sql = "SELECT " + catalogue.getAlias() + ".CURRENCY, SUM(" + catalogueNumber.getAlias() + ".CATALOGUEVALUE) AS VALUE ";
-                sql += "FROM " + stamp.getTableName() + ' AS ' + stamp.getAlias() + ' ';
-                sql += 'JOIN ' + catalogueNumber.getTableName() + ' AS ' + catalogueNumber.getAlias() + ' ON ' + stamp.getAlias() + '.ID=' + catalogueNumber.getAlias() + '.STAMP_ID ';
-                sql += "LEFT JOIN " + ownership.getTableName() + ' AS ' + ownership.getAlias() + ' ON ' + stamp.getAlias() + '.ID=' + ownership.getAlias() + '.STAMP_ID ';                
-                sql += "LEFT JOIN " + catalogue.getTableName() + ' AS ' + catalogue.getAlias() + ' ON ' + catalogueNumber.getAlias() + '.CATALOGUE_REF = ' + catalogue.getAlias() + '.ID ';
+                sql += generateFromTables();
                 sql += "WHERE " + catalogueNumber.getAlias() + ".ACTIVE=1 ";
                 var whereClause = ($filter) ? dataTranslator.toWhereClause($filter, [stamp, catalogueNumber, catalogue, ownership]) : '';
                 if (whereClause.length > 0) {
@@ -38,7 +43,13 @@ var report = function () {
                     var processResults = function () {
                         var sum = 0.0;
                         _.each(results, function (result) {
-                            sum += fx.convert(result.VALUE, { from: result.CURRENCY, to: currency });
+                            if( result.VALUE && result.VALUE > 0 ) {
+                                var cur = result.CURRENCY;
+                                if( !cur || cur === '' ) {
+                                    cur = 'USD';
+                                }
+                                sum += fx.convert(result.VALUE, { from: cur, to: currency });
+                            }
                         });
                         var value = accounting.toFixed(sum, 2);
                         defer.resolve(value);
@@ -50,7 +61,37 @@ var report = function () {
         },
         getCostBasis: function ($filter, currency) {
             var defer = q.defer();
-            defer.resolve(20.0);
+            connectionManager.getConnection("reports").then(function (connection) {
+                var sql = "SELECT " + ownership.getAlias() + ".CURRENCY, SUM(" + ownership.getAlias() + ".PRICE) AS VALUE ";
+                sql += generateFromTables();
+                sql += "WHERE " + stamp.getAlias() + ".WANTLIST=0 ";
+                var whereClause = ($filter) ? dataTranslator.toWhereClause($filter, [stamp, catalogueNumber, catalogue, ownership]) : '';
+                if (whereClause.length > 0) {
+                    sql += "AND " + whereClause + " ";
+                }
+                sql += "GROUP BY " + ownership.getAlias() + ".CURRENCY";
+                sqlTrace.debug(sql);
+                var query = connection.query(sql, function (err, results) {
+                    if (err) {
+                        defer.reject(dataTranslator.getErrorMessage(err));
+                    }
+                    var processResults = function () {
+                        var sum = 0.0;
+                        _.each(results, function (result) {
+                            if( result.VALUE && result.VALUE > 0 ) {
+                                var cur = result.CURRENCY;
+                                if( !cur || cur === '' ) {
+                                    cur = 'USD';
+                                }
+                                sum += fx.convert(result.VALUE, { from: cur, to: currency });
+                            }
+                        });
+                        var value = accounting.toFixed(sum, 2);
+                        defer.resolve(value);
+                    };
+                    ExchangeRates.checkRates(processResults);
+                });
+            });
             return defer.promise;
         },
         getCashValue: function ($filter, currency) {
